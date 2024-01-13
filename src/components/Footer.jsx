55c80c2a487480ecebb73e169ecb28e9cec6addc
crxng <45475315+crxng@users.noreply.github.com>
2024-01-13 02:18:58 -0500

    committed project to git

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+      <>
+        <footer>
+          <div className="footer-content">
+            <div className="footer-content__text">
+              <p>Privacy</p>
+              <p>Terms of Use</p>
+            </div>
+          </div>
+        </footer>
+      </>
+    );
+  }
+  
+  export default Footer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import MarketUpdate from "../components/MarketUpdate";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <MarketUpdate />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
